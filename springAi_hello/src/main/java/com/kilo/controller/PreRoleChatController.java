1e6211ba71ccbc463431f87aa6d947c62d8b9b6c
kilo <15990037660@163.com>
2025-06-10 00:21:28 +0800

    实现流式响应

--- springAi_hello/src/main/java/com/kilo/controller/PreRoleChatController.java
+++ springAi_hello/src/main/java/com/kilo/controller/PreRoleChatController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class PreRoleChatController {
@@ -15,5 +16,15 @@ public class PreRoleChatController {
         String response = chatClient.prompt().user(message).call().content();
         System.out.println("response : " + response);
         return response;
+    }
+
+    /**
+     * 流式响应
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/chatai/stream", produces = "text/html;charset=UTF-8")
+    public Flux<String> chatAiStream (@RequestParam(value = "msg") String message) {
+        return chatClient.prompt().user(message).stream().content();
     }
 }
