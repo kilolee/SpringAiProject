288969529666b30c13f877553a622d7836c23367
kilo <15990037660@163.com>
2025-06-15 13:39:10 +0800

    使用alibaba的通义千问大模型api

---
+++ springAi_alibaba/src/main/java/com/kilo/SpringAiAlibabaApplication.java
@@ -0,0 +1,11 @@
+package com.kilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiAlibabaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiAlibabaApplication.class, args);
+    }
+}
